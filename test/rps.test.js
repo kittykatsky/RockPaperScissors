ef85075197aefe45dc461b025c4972a7a1967d0e
kittykatsky <kd.work65@gmail.com>
2020-10-16 18:29:43 +0100

    [test] KD - 16.10.20 - T1  adding failing tests for setting up and running a game of RPS
     Features Added:
     - added failing test for setting up a game of RPS
     - added failing test for playing a game of RPS
     - added failing tests for testsing ownership and flow of contracts
    
     TODO:
     - pass failing tests
    
     On branch tests
     Changes to be committed:
    	modified:   test/rps.test.js

--- test/rps.test.js
+++ test/rps.test.js
@@ -14,13 +14,26 @@ var chaiAsPromised = require("chai-as-promised");
 chai.use(chaiAsPromised);
 const expect = chai.expect;
 const truffleAssert = require('truffle-assertions');
+const RockPaperScissors = artifacts.require('RockPaperScissors');
 const timeMachine = require('ganache-time-traveler');
 
 require("dotenv").config({path: "./.env"});
 
-contract('RockPaperScissor', function(accounts) {
+contract('RPS', function(accounts) {
 
-    beforeEach('Setup new Remittance before each test', async function () {
+    let RPS, trx, snapshotId;
+    const [aliceAccount, bobAccount, carolAccount] = accounts;
+
+    const player1 = bobAccount;
+    const player2 = carolAccount;
+    const secret = fromAscii('test');
+    const cost = 1000;
+    const wager = 5000;
+
+    beforeEach('Setup new RPS before each test', async function () {
+        RPS = await RockPaperScissors.new(false, cost, {from: aliceAccount});
+        gameId = await RPS.generateGameId(secret, 'ROCK', {from: player1})
+        trx = await RPS.newGame(gameId, 50, {from: player1, value: wager});
         snapshotId = (await timeMachine.takeSnapshot())['result'];
     });
 
@@ -35,40 +48,85 @@ contract('RockPaperScissor', function(accounts) {
     describe('Pausable', function () {
 
         it("Should be owned by the deployer", async function () {
-            return expect(await remittance.getOwner()).to.equal(aliceAccount)
+            return expect(await RPS.getOwner()).to.equal(aliceAccount)
         });
 
-        it("Should not be possible to withdraw when paused", async function () {
-            await remittance.pause({from: aliceAccount})
-            return expect(remittance.releaseFunds(puzzle, {from: converter})).to.be.rejected;
+        it("Should not be possible to host a game when paused", async function () {
+            await RPS.pause({from: aliceAccount})
+            return expect(RPS.newGame(player2, {from: player1, value: wager})).to.be.rejected;
         });
 
         it("Should be possible to kill a paused contract", async function () {
-            await remittance.pause({from: aliceAccount});
-            const tx = await remittance.kill({from: aliceAccount});
+            await RPS.pause({from: aliceAccount});
+            const tx = await RPS.kill({from: aliceAccount});
             return assert.strictEqual(tx.receipt.status, true);
         });
 
         it("Should no be possible to run a killed contract", async function () {
-            await remittance.pause({from: aliceAccount});
-            return expect(remittance.releaseFunds(puzzle, {from: converter})).to.be.rejected;
+            await RPS.pause({from: aliceAccount});
+            return expect(RPS.playGame(gameId, 'PAPER', {from: player1})).to.be.rejected;
         });
 
         it("Should not be possible to unpause a killed contract", async function () {
-            await remittance.pause({from: aliceAccount});
-            await remittance.kill({from: aliceAccount});
-            return expect(remittance.resume({from: aliceAccount})).to.be.rejected;
+            await RPS.pause({from: aliceAccount});
+            await RPS.kill({from: aliceAccount});
+            return expect(RPS.resume({from: aliceAccount})).to.be.rejected;
         });
 
         it("Should not be possible to empty a live contract", async function () {
-            return expect(remittance.emptyAccount(aliceAccount, {from: aliceAccount})).to.be.rejected;
+            return expect(RPS.emptyAccount(aliceAccount, {from: aliceAccount})).to.be.rejected;
         });
 
         it("Should be possible to empty a killed contract", async function () {
-            await remittance.pause({from: aliceAccount});
-            await remittance.kill({from: aliceAccount});
-            return expect(remittance.emptyAccount(aliceAccount, {from: aliceAccount})).to.be.fulfilled;
+            await RPS.pause({from: aliceAccount});
+            await RPS.kill({from: aliceAccount});
+            return expect(RPS.emptyAccount(aliceAccount, {from: aliceAccount})).to.be.fulfilled;
         });
     });
 
+    describe('Play game', function () {
+
+        it("Should not be possible to host a game when one is ongoing", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should be possible to join a hosted game", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should not be possible to join a game if not providing a minimum stake", async function () {
+            done(new Error("Write test"));
+        });
+
+        it("Should not be possible to join a game in progress", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should not be possible to submit a move if not part of the game", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should be possible to submit a move", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should not be possible to submit a second move", async function (done) {
+            done(new Error("Write test"));
+        });
+    });
+
+    describe('Game logic', function () {
+        it("Should time out and return funds to host if player 2" +
+            "doesnt make a move before a given amount of time", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should add the total amount (minus fees) to the winner", async function (done) {
+            done(new Error("Write test"));
+        });
+
+        it("Should allow user to be their previous winnings", async function (done) {
+            done(new Error("Write test"));
+        });
+    });
 });
